fb0cc2b45241a850a3274dafd84e818ea0477cc5
Aram <aramdavtyan86@gmail.com>
2022-09-30 19:40:16 +0400

    Implementation of file upload functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'task';
+export class AppComponent{
+  title = 'dropzone';
+  files: File[] = [];
+  allowedTypes = 'text/plain';
+  allowedSize = 1000000;
+  constructor() { }
+
+  onChange(event: { addedFiles: any; }) {
+    this.files.push(...event.addedFiles);
+
+    const formData = new FormData();
+
+    for (let i = 0; i < this.files.length; i++) {
+      formData.append("file[]", this.files[i]);
+    }
+  }
+
+  onRemove(event: File) {
+    this.files.splice(this.files.indexOf(event), 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {NgxDropzoneModule} from "ngx-dropzone";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxDropzoneModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
